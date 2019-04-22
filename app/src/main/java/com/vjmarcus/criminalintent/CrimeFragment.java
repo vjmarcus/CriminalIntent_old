912921e59462282060dab0cc06a02d181f841497
vjmarcus <vjmarcus1@gmail.com>
2019-04-22 14:49:23 +0300

    Create CrimeFragment

---
+++ app/src/main/java/com/vjmarcus/criminalintent/CrimeFragment.java
@@ -0,0 +1,16 @@
+package com.vjmarcus.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+}
